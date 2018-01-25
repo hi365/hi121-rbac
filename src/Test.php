abe61d44789923bf509cbccba09c6c8f13a8efd1
kelei <5735570@qq.com>
2018-01-26 03:46:56 +0800

    Default

---
+++ src/Test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: www
+ * Date: 18-1-26
+ * Time: 上午3:42
+ */
+namespace hi121\rbac;
+
+class Test{
+    public static function run(){
+        echo 123;
+    }
+}
